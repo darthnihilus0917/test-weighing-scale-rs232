cddf6a6cd60d405ab18d6db126dfec61ee1d110b
darthnihilus0917 <jcsantosdevtest@gmail.com>
2024-02-26 13:36:59 +0800

    Updated tester

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const { SerialPort } = require('serialport');
-const Readline = require('@serialport/parser-readline');
+const { ReadlineParser } = require('@serialport/parser-readline')
 
  // Replace 'COM1' with your serial port path
 const SERIAL_PORT = 'COM1';
 const BAUD_RATE = 9600;
+const DATA_BITS = 8;
+const STOP_BITS = 1;
 
-const port = new SerialPort(
-        SERIAL_PORT, 
-        { baudRate: BAUD_RATE }
-    );
+const port = new SerialPort({ 
+  path: SERIAL_PORT,
+  baudRate: BAUD_RATE,
+  dataBits: DATA_BITS,
+  stopBits: STOP_BITS,
+});
 
-const parser = port.pipe(new Readline({ delimiter: '\n' }));
+const parser = port.pipe(new ReadlineParser({ delimiter: '\n' }));
 
 port.on('open', () => {
   console.log('Serial port is open');
